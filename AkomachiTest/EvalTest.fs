74e864d261a9e937ae97d55782bb4b96b88f8c7b
psi <ledyba@users.sourceforge.jp>
2014-03-14 03:38:40 +0900

    renamed

--- AkomachiTest/EvalTest.fs
+++ AkomachiTest/EvalTest.fs
@@ -13,7 +13,7 @@ type EvalTest() =
     member this.TestEval() =
       match (run Parser.prog "1+2+3") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 6) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
@@ -21,7 +21,7 @@ type EvalTest() =
     member this.TestFunc() =
       match (run Parser.prog "var z = fun (x, y) x+y; z(1,2)") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 3) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
@@ -31,7 +31,7 @@ type EvalTest() =
     member this.TestIf() =
       match (run Parser.prog "var z = fun (x, y) x+y; if z(1,2) == 3 then 1 else 2") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 1) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
@@ -41,7 +41,7 @@ type EvalTest() =
     member this.TestObj() =
       match (run Parser.prog "var obj = {v->1, b->true}; if obj.b then 1 else 2") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 1) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
@@ -52,7 +52,7 @@ type EvalTest() =
       (*
       match (run Parser.prog "var t = fun (x, y, z) if x <= y then y else t(t(x-1,y,z), t(y-1,z,x), t(z-1,x,y)); t(12,6,0)") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 12) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
@@ -62,7 +62,7 @@ type EvalTest() =
     member this.TestSelf() =
       match (run Parser.prog "var obj = { fn -> fun (x, y, z) self.x, x->1 }; obj.fn(1,2,3);") with
         | Success (r,us,p)   ->
-             match Stage.World().dance r with
+             match Akomachi().dance r with
                 | (Value.Int 1) -> ()
                 | x -> Assert.Fail(sprintf "%A" x)
         | Failure (msg,err,us) -> Assert.Fail(sprintf "Failed to parse: %s" msg); Value.Null |> ignore
